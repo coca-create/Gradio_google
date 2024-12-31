f92a4a858bcfe311dcc945b553606d5462b4693f
Coca2024 <himotoki.sansu@gmail.com>
2024-12-31 21:39:03 +0900

    delete

--- PeriOz_web.py
+++ PeriOz_web.py
@@ -2,40 +2,12 @@
 import os
 import sys
 import gradio as gr
-from gradio_components import gr_components as gc
+from modules import gr_components as gc
 from datetime import datetime
 import multiprocessing
 
-#import warnings
 
-#warnings.filterwarnings("ignore", category=FutureWarning)
-#print("Current working directory:", os.getcwd())
-# 必要なら作業ディレクトリをスクリプトのディレクトリに変更
-'''script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir)
 
-def get_base_path():
-    if getattr(sys, 'frozen', False):
-        return os.path.dirname(sys.executable)
-    else:
-        return os.path.dirname(os.path.abspath(__file__))
-
-base_path = get_base_path()'''
-
-#current_directory = os.getcwd()
-#print(f"Current directory: {current_directory}")
-#cuda_path = os.path.join(base_path,"cuda")
-##ffmpeg_path = os.path.join(base_path,"bin")
-#ffprobe_path = os.path.join(base_path,"bin")
-#python_path = os.path.join(base_path, "python311")
-
-#os.environ["FFMPEG_PATH"] = ffmpeg_path
-#os.environ["FFPROBE_PATH"] = ffprobe_path
-#os.environ["PYTHON_PATH"] = python_path
-#os.environ["CUDA_PATH"] = cuda_path
-#os.environ["CUDNN_PATH"] = cudnn_path
-
-#os.environ["PATH"] = cuda_path + ";" + cudnn_path + ";" + ffmpeg_path + ";" + ffprobe_path + ";" + os.environ["PATH"]
 css="""
 
     .my-table-container {
